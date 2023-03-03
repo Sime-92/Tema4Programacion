035a2935714493d85ec9968fb5ce9857f016a5aa
Simeon-Paris <alupar9905@ieselcaminas.org>
2023-03-03 21:35:29 +0100

    subo pprimer ejercicio, en los apuntes pone una cosa y en la tarea otra, por lo que opto por hacer lo que pone en la tarea

---
+++ src/Tablas.java
@@ -0,0 +1,11 @@
+public class Tablas {
+    public static void main(String[] args) {
+
+
+        int meses [] = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int i= meses[9]; i>= meses[0];i-- ){
+            System.out.println(i);
+        }
+    }
+}
